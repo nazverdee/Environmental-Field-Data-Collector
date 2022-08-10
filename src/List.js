576d0ed2c8c97aeefa759db66c1e2e9f8c765277
Nazareth Villa <vnazareth6@gmail.com>
2022-08-09 23:20:25 -0500

    incorporated Read operations

--- src/List.js
+++ src/List.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./List.css";
+import { Table } from "semantic-ui-react";
 
-export default function List(props) {
+export default function List() {
+  const [APIData, setAPIData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://62f3115118493ca21f3d133f.mockapi.io/CityForm`)
+      .then((response) => {
+        setAPIData(response.data);
+      });
+  }, []);
+
   return (
-    <div>
-      <div>{props.children}</div>
+    <div id="main">
+      <Table singleLine id="tables">
+        <Table.Header className="thead">
+          <Table.Row className="headTr">
+            <Table.HeaderCell>City</Table.HeaderCell>
+            <Table.HeaderCell>AQI</Table.HeaderCell>
+            <Table.HeaderCell>Noise</Table.HeaderCell>
+            <Table.HeaderCell>Pollutant</Table.HeaderCell>
+            <Table.HeaderCell>Comment</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {APIData.map((data) => {
+            return (
+              <Table.Row key={data.newCityValue} className="rows">
+                <Table.Cell>{data.newCityValue}</Table.Cell>
+                <Table.Cell>{data.AQI.value}</Table.Cell>
+                <Table.Cell>{data.noise.value}</Table.Cell>
+                <Table.Cell>{data.pollutants.value}</Table.Cell>
+                <Table.Cell>{data.comment}</Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -3,25 +3,22 @@ import CityForm from "./CityForm";
 import List from "./List";
 import "./Main.css";
 import ButtonCityForm from "./ButtonCityForm";
-import Items from "./Items.js";
-import ListHeader from "./ListHeader";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function Main() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="row">
+    <Router className="row">
       <div id="recentCitiesAdded">
         <section className="cityTopSection">
           <h1 id="listtitle">Cities Added List</h1>
           <ButtonCityForm setIsOpen={setIsOpen} />
         </section>
+        <div></div>
         {isOpen && <CityForm setIsOpen={setIsOpen} />}
-        <ListHeader />
-        <List>
-          <Items />
-        </List>
+        <List />
       </div>
-    </div>
+    </Router>
   );
 }
